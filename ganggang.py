99eb3c75a30206fcf8c387985c61edabde6c61ef
baroganggang <hoangchuonggiabao@icloud.com>
2026-03-11 08:57:38 +0200

    Refactor random selection of messages in ganggang.py

--- ganggang.py
+++ ganggang.py
@@ -1,12 +1,8 @@
 from random import randint
 
+x = ["Bao on komea","Toivo on homo","Davut on homo"]
 
 for i in range(10):
-    gang = randint(1,3)
-
-    if gang == 1: 
-        print("Davut on homo")
-    elif gang == 2:
-        print("Meerab on homo")
-    elif gang == 3:
-        print("Bao on komea")
+    gang = random.choice(x)
+    print(gang)
+   
